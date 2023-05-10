9dfae9a3fef4b129efea6d7b69b888724fad327c
burgdie01 <dburgstaller@gmx.de>
2023-05-10 08:30:34 +0200

    9 using laravel queriy builder

--- app/Models/Book.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Models;
-
-class Book extends Model
-{
-    protected $table = 'my-books';
-}
--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,60 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    // fetch all users
+    //$users = DB::select("select * from users");
+    //$users = DB::select("select * from users where id=1");
+    //$users = DB::select("select * from users where email=?", ['dburgstaller@gmx.de']);
+
+    /**
+     * Create new users
+     */
+    // $user = DB::insert('insert into users(name, email, password) values(?,?,?)', [
+    //     'Sarthak',
+    //     'sarthak1@bitfumes.com',
+    //     'password',
+    // ]);
+    /**
+     * Update
+     */
+
+    //$user = DB::update("update users set email ='abc@bitfumes.com' where id=4");
+
+    // $user = DB::update("update users set email = ? where id=?", [
+    //     'dieter@bitfumes.com',
+    //     4,
+    // ]);
+    /**
+     * Delete a user
+     */
+    //$user = DB::delete("delete from users where id=4");
+
+    /**
+     * Query Builder
+     */
+    //Fetch all users
+    // $users = DB::table('users')->get();
+    //$users = DB::table('users')->where('id', 7)->first();
+    $users = DB::table('users')->find(7);
+    //$users = DB::table('users')->where('id', 1)->get();
+
+    //Insert Data
+    // $user = DB::table('users')->insert([
+    //     'name' => 'Dieter',
+    //     'email' => 'burgdi@gmail.com',
+    //     'password' => 'password',
+    // ]);
+
+    // $user = DB::table('users')->where('id', 5)->update([
+    //     'email' => 'abc@gmail.com',
+    // ]);
+    // Delete a user
+    // $user = DB::table('users')->where('id', 5)->delete();
+
+    dd($users);
 });
 
 Route::get('/dashboard', function () {
@@ -28,4 +78,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
